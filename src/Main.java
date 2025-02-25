f58f603b2e4e1b535137444d86e0a1538bc76c6b
Slashaershov <127024236+Slashaershov@users.noreply.github.com>
2025-02-25 13:37:07 +0300

    fill

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,54 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
+        //ex 1
+        for (int i=1; i<=10; i++ )
+        {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        //ex 2
+        for (int i=10; i>=1; i-- )
+        {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        //ex 3
+        for (int i=0; i<=17; i+=2 )
+        {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        //ex 4
+        for (int i=10; i>=-10; i-- )
+        {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        //ex 5
+        for (int year=1904; year<=2096; year+=4 )
+        {
+            System.out.println(year + " год является високосным");
+        }
+        System.out.println();
+
+        //ex 6
+        for (int i=7; i<100; i+=7 )
+        {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        //ex 7
+
+        for (int i=1; i<1000; i*=2 )
+        {
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
 }
